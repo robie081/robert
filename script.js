747f82eb96865cecb00bbdca6088bc2bf2ac7853
robie081 <robert.lantus@icloud.com>
2023-10-16 17:50:17 +0300

    building 7

--- script.js
+++ script.js
@@ -17,6 +17,28 @@ btn.addEventListener('click', () => {
     ul.classList.toggle('open');
 });
 
+// Vertical Bar in Front of Menu Items
+
+// Get all list items and their associated links
+const listItems = document.querySelectorAll('ul li');
+const links = document.querySelectorAll('ul li a');
+
+// Add a click event listener to each link
+links.forEach((link, index) => {
+  link.addEventListener('click', (event) => {
+    // Prevent the default link behavior
+    event.preventDefault();
+    
+    // Remove the "active" class from all list items
+    listItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    // Add the "active" class to the clicked list item
+    listItems[index].classList.add('active');
+  });
+});
+
 // Sticky Menu GPT
 
 // Sticky Menu
